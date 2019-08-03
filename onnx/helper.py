9fc7ca89af9ccd58d26ab29635168a80163363d9
JiyuanServer <jiyuan@kneron.us>
2019-08-03 14:37:19 -0700

    Enable None value attributes

--- onnx/helper.py
+++ onnx/helper.py
@@ -246,8 +246,10 @@ def make_attribute(
                 "You passed in an iterable attribute but I cannot figure out "
                 "its applicable type.")
     else:
-        raise ValueError(
-            'Value "{}" is not valid attribute data type.'.format(value))
+        attr.s = _to_bytes_or_false("None")
+        attr.type = AttributeProto.STRING
+        # raise ValueError(
+        #     'Value "{}" is not valid attribute data type.'.format(value))
     return attr
 
 
